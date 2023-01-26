da4d0b7583c9ceb7d06850d42b474eeed3556e58
bkarabo754 <bkmasango26@gmail.com>
2023-01-26 17:52:18 +0200

    Updating the Header Section

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,7 +20,7 @@ const Header = () => {
     })
   })
   return (
-    <header className={`${isActive ? 'h-[100px] lg:h-[110px] shadow-lg' : 'h-[120px] lg:h-[150px]'} fixed left-0 right-0 top-0 z-10 max-w-[1920px] w-full mx-auto transition-all duration-300`}>
+    <header className={`${isActive ? 'h-[100px] lg:h-[110px] shadow-lg' : 'h-[120px] lg:h-[150px]'} fixed bg-white left-0 right-0  z-10 max-w-[1920px] w-full mx-auto transition-all duration-300`}>
     <div className='flex justify-between items-center h-full pl-[50px] pr-[60px]'>
         <a href="/">
           <img className="w-[188px] h-[90px]" src={logo} alt="" />
@@ -29,11 +29,14 @@ const Header = () => {
             <Nav />
         </div>
 
-        <div className='xl:hidden basolute right-[5%] bg-dark text-white p-2 rounded-md cursor-pointer'>
+        <div  onClick={() => setNavMobile(!navMobile)} className='xl:hidden absolute right-[5%] bg-dark text-white p-2 rounded-md cursor-pointer'>
           <TiThMenuOutline className="text-3xl"/>
         </div>
-        <div className="fixed bg-red-500 w-full h-full left-0 -z-10 transition-all duration-300">
+        <div className={`${navMobile ? 'max-h-full' : 'max-h-0'} ${isActive ? 'top-[100px] lg:top-[110px]' : 'top-[120px] lg:top-[150px]'} fixed bg-white w-full h-full left-0 -z-10 transition-all duration-300`}>
           <NavMobile />
+        </div>
+        <div className="hidden xl:flex">
+            <Socials />
         </div>
     </div>
     </header>
--- src/components/NavMobile.js
+++ src/components/NavMobile.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import {navData} from '../data'
+import Socials from './Socials';
 
 const NavMobile = () => {
-  return <div>NavMobile</div>;
+  const { items } = navData
+
+  return (
+    <nav className="w-full h-full flex flex-col justify-evenly overflow-hidden">
+      <ul className="flex flex-col justify-center items-center gap-y-6 py-6 mb-8">
+        {items.map((item, index) => {
+          return <li key={index}>
+            <a className="text-2xl font-primary uppercase" href={item.href}>{item.name}</a>
+          </li>
+        })}
+      </ul>
+      <div className="text-2xl">
+        <Socials />
+      </div>
+    </nav>
+    );
 };
 
 export default NavMobile;
