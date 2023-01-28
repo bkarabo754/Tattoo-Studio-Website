8c49f7392470b615aaf2aa729a8781d2d490a738
bkarabo754 <bkmasango26@gmail.com>
2023-01-28 18:26:34 +0200

    Copyright section

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -1,7 +1,29 @@
 import React from 'react';
+// import motion
+import { motion } from 'framer-motion';
 
 const Copyright = () => {
-  return <div>Copyright</div>;
+  return (
+    <div className='bg-dark py-6 border-t border-[#2B2B2B] text-[#DBDBDB]'>
+      <div className='container mx-auto'>
+        <motion.div
+          initial={{ y: 40, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{
+            type: 'tween',
+            duration: 0.8,
+            delay: 0.2,
+          }}
+          viewport={{ once: false, amount: 0 }}
+          className='tracking-[0.02em] text-base'
+        >
+          &copy; 2022{' '}
+          <span className='font-semibold text-white cursor-pointer'><a href="/">The Void Tattoo.</a></span>{' '}
+          All rights reserved.
+        </motion.div>
+      </div>
+    </div>
+  );
 };
 
 export default Copyright;
